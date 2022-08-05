4b453ee6d58f392b9627ca6c9a8973791bfd8597
Sulash Manandhar <sulash.manandhar@gmail.com>
2022-08-05 14:42:18 +0545

    Initial REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,31 +1,60 @@
 const asyncHandler = require("express-async-handler");
-
+const Goal = require("../models/goalModel");
 //@desc Get goals
 //@route Get/api/goals
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
+  const goal = await Goal.find();
+
+  res.status(200).json(goal);
 });
 
 //@desc Set goals
 //@route POST/api/goals
 const setGoal = asyncHandler(async (req, res) => {
-  if (!req.body.goal) {
+  if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a goal.");
   }
-  res.status(200).json({ message: "Create Goals" });
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 //@desc Update goals
 //@route PUT/api/goals/:id
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error(`Goal with id ${req.params.id} is not found.`);
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 //@desc Delete goals
 //@route Delete/api/goals/:id
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error(`Goal with id ${req.params.id} is not found.`);
+  }
+  const deletedGoal = await goal.remove();
+
+  if (!deletedGoal) {
+    return res
+      .status(500)
+      .json({ message: `Failed to delete goal with id ${req.params.id}` });
+  }
+  return res
+    .status(200)
+    .json({ message: `Successfully deleted goal with id ${req.params.id}` });
 });
 
 module.exports = {
