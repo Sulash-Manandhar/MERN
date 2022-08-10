084aa36eed1ccdefbec62874bff91224b8d7e77d
Sulash Manandhar <sulash.manandhar@gmail.com>
2022-08-10 20:59:57 +0545

    Backend User and  Goal API complete

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 //@desc Get goals
 //@route Get/api/goals
 const getGoals = asyncHandler(async (req, res) => {
-  const goal = await Goal.find();
+  const goal = await Goal.find({ user: req.user.id });
 
   res.status(200).json(goal);
 });
@@ -18,6 +19,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
   res.status(200).json(goal);
 });
@@ -29,6 +31,19 @@ const updateGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error(`Goal with id ${req.params.id} is not found.`);
+  }
+
+  const user = await User.findById(req.user.id);
+  //check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  //Logged   in user  matches the goal  user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User  not  authorized");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -45,6 +60,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error(`Goal with id ${req.params.id} is not found.`);
   }
+
+  const user = await User.findById(req.user.id);
+  //check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  //Logged   in user  matches the goal  user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User  not  authorized");
+  }
+
   const deletedGoal = await goal.remove();
 
   if (!deletedGoal) {
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 
 console.log(process.env.MONGO_URI);
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
